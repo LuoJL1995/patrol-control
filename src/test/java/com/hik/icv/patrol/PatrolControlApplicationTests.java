1940b21afe9018c677d4fed302f1eb8809241b2b
luojialei <luojialei95@163.com>
2020-06-05 13:23:54 +0800

    luojialei commit from  hik

--- src/test/java/com/hik/icv/patrol/PatrolControlApplicationTests.java
+++ src/test/java/com/hik/icv/patrol/PatrolControlApplicationTests.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(classes = Application.class)
-class PatrolCloudApplicationTests {
+class PatrolControlApplicationTests {
 
     @Test
     void contextLoads() {
