d0640693254948cd101c4cd98bc31889260b38d7
luojialei <luojialei95@163.com>
2020-06-15 08:29:41 +0800

    luojialei commit from  hik

--- src/main/java/com/hik/icv/patrol/netty/SerialMsgEncoder.java
+++ src/main/java/com/hik/icv/patrol/netty/SerialMsgEncoder.java
@@ -3,8 +3,6 @@ package com.hik.icv.patrol.netty;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToByteEncoder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * @Description netty自定义解码器解决byte消息粘包情况
